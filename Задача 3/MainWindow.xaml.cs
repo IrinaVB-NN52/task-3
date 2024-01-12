7391ed7769db44873ad4169d1927b62ff66f62d0
Ирина Баранова <irina.baranova14@outlook.com>
2024-01-12 23:05:55 +0300

    Добавлен словарь

--- Задача 3/MainWindow.xaml.cs
+++ Задача 3/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace Задача_3
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontName = ((sender as ComboBox).SelectedItem as string);
             if (textBox != null)
             {
                 textBox.FontFamily = new FontFamily(fontName);
@@ -38,9 +38,10 @@ namespace Задача_3
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            if(textBox != null )
+            double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem));
+            if (textBox != null )
             {
-                double fontSize = Convert.ToDouble(((sender as ComboBox).SelectedItem as TextBlock).Text);
+                
                 textBox.FontSize =fontSize;
             }
         }
